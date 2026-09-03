0659f7edf0c75480e2fc8ea59cd378f81b1ab59e
LeslyLopez1 <158329281+LeslyLopez1@users.noreply.github.com>
2026-09-03 11:13:00 -0700

    Add files via upload

---
+++ Juego21Controlador.java
@@ -0,0 +1,54 @@
+package blackjack.practica1_alg20262.Controlador;
+
+import blackjack.practica1_alg20262.Modelo.Juego21;
+import blackjack.practica1_alg20262.Vista.Juego21FX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Juego21Controlador extends Application {
+
+    private Juego21 modelo;
+    private Juego21FX vista;
+
+    @Override
+    public void start(Stage stage) {
+        modelo = new Juego21();
+
+        vista = new Juego21FX();
+
+        conectarBotones();
+        vista.actualizarPantalla(modelo);
+
+        stage.setTitle("Blackjack 21");
+        stage.setScene(vista.getEscena());
+        stage.show();
+    }
+
+    private void conectarBotones() {
+        vista.getPedirBtn().setOnAction(evento -> pedirCarta());
+        vista.getPlantarseBtn().setOnAction(evento -> quedarse());
+        vista.getNuevo2Btn().setOnAction(evento -> nuevoJuego(2));
+        vista.getNuevo3Btn().setOnAction(evento -> nuevoJuego(3));
+        vista.getNuevo4Btn().setOnAction(evento -> nuevoJuego(4));
+    }
+
+    private void pedirCarta() {
+        modelo.pedirCarta();
+        vista.actualizarPantalla(modelo);
+    }
+
+    private void quedarse() {
+        modelo.quedarse();
+        vista.actualizarPantalla(modelo);
+    }
+
+    private void nuevoJuego(int cantidadJugadores) {
+        modelo.iniciarNuevoJuego(cantidadJugadores);
+        vista.actualizarPantalla(modelo);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
