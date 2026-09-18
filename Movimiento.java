6fab04aeaeed6149448b36c141b83e4c55c40072
LeslyLopez1 <158329281+LeslyLopez1@users.noreply.github.com>
2026-09-18 09:18:46 -0700

    Add files via upload

---
+++ Movimiento.java
@@ -0,0 +1,24 @@
+package blackjack.practica1_alg20262.Modelo;
+
+public class Movimiento {
+    private String tipo;
+    private int jugador;
+
+    public Movimiento(String tipo) {
+        this.tipo = tipo;
+        this.jugador=jugador;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getJugador() {
+        return jugador;
+    }
+
+    @Override
+    public String toString() {
+        return tipo + "( Jugador" + (jugador + 1) + ")";
+    }
+}
